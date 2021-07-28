c4ff21ed3ee382d051c4f206eec2463f6e43b4b0
PRIME <86994680+p-r-i-m-e@users.noreply.github.com>
2021-07-28 11:30:51 +0000

    Add files via upload

---
+++ prime.py
@@ -0,0 +1,85 @@
+print(""" 
+\033[0;33m  111111  111111  111111  111    111  111111 
+\033[0;32m  11  11  11  11    11    11 1111 11  11
+\033[0;36m  111111  1111      11    11  11  11  111111
+\033[1;33m  11      11 11     11    11  11  11  11
+\033[0;31m  11      11  11  1111111 11  11  11  111111 
+                                            Prime Family!
+                                    Th Ru!
+""")
+print("")
+print(" \033[0;32m ")
+user=str(input("Enter Tool Name : "))
+print("")
+password=str(input("\033[0;35mEnter Tool Password : "))
+if user=="0" and password=="0":
+  print( "\033[0;36m ")
+  def load():
+   import time as t
+   t.spleep(0.3)
+  for x in range(0,10,1):
+   import time as a
+   a.sleep(0.2)
+   print(" loading..")
+   x+=1
+  import time as a
+  a.sleep(0.2)
+  print("""
+  Choose Your Opition
+
+  1.Termux Packges
+
+  2.Other Tools
+
+  3.Dh Hackbar
+
+  4.Folliw Page
+  
+  5.Exit """)
+  a.sleep(0.2)
+  print("")
+  choice=int(input("Enter Choice :"))
+  try:
+    if choice==1:
+      import os
+      os.system("pkg update -y ")
+      os.system("pkg upgrade -y")
+      os.system("pkg install python3 -y")
+      os.system("pkg install wget")
+      os.system("pkg install bash")
+      os.system("pkg install openssh")
+      os.system("pkg install wget")
+      os.system("pkg install pip")
+      os.system("pkg install pip2")
+      os.system("pip2 install requests")
+      os.system("pip2 install mechanize")
+      os.system("pkg install php")
+      os.system("pip2 install lolcat")
+      os.system("apt install toilet")
+      os.system("pkg install git -y")
+      os.system("pip install lolcat")
+      os.system("pip3 install requests")
+      os.system("pip3 install colorama")
+      os.system("pkg install nano")
+    elif choice==2:
+      import os
+      os.system("git clone https://github.com/AngelSecurityTeam/Cam-Hackers")
+      os.system("git clone https://github.com/BotolMehedi/bint")
+      os.system("git clone https://github.com/sqlmapproject/sqlmap")
+    elif choice==3:
+      import os
+      os.system("xdg-open https://github.com/darknethaxor/DH-HackBar/tree/v1.1")
+    elif choice==4:
+      import os
+      os.system("xdg-open https://www.facebook.com/prime.127.0.0.1.007/")
+    elif choice==5:
+      import os
+      os.system("cd $HOME")
+    else:
+      print("\033[0;31m Your Choice Not Include!")
+  except ValueError:
+   print("\033[0;31m Enter the number only")
+else:
+  import time as a
+  a.sleep(0.3)
+  print(" \033[0;31m Password Wrong")
